a423b5e79cf511e58518f6b184f86f6c15aa63f9
dipiert <dipiert@gmail.com>
2018-03-15 09:37:01 -0300

    Add dql

---
+++ sql/dql.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 15-03-2018 a las 09:35:14
+-- Versión del servidor: 5.7.21-0ubuntu0.16.04.1
+-- Versión de PHP: 7.1.12-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hojaruta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id` tinyint(3) UNSIGNED NOT NULL,
+  `estado` varchar(30) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_item`
+--
+
+CREATE TABLE `estado_item` (
+  `nro_inventario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `id_estado` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `item`
+--
+
+CREATE TABLE `item` (
+  `autor` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `titulo` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `nro_inventario` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimientos`
+--
+
+CREATE TABLE `movimientos` (
+  `id_responsable` tinyint(3) UNSIGNED ZEROFILL NOT NULL,
+  `fecha` date NOT NULL,
+  `nro_inventario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `id_estado_anterior` tinyint(3) UNSIGNED NOT NULL,
+  `id_estado_nuevo` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` tinyint(3) UNSIGNED ZEROFILL NOT NULL,
+  `usuario` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `contrasena` varchar(32) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estado_item`
+--
+ALTER TABLE `estado_item`
+  ADD PRIMARY KEY (`nro_inventario`),
+  ADD KEY `id_estado` (`id_estado`);
+
+--
+-- Indices de la tabla `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`nro_inventario`);
+
+--
+-- Indices de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD PRIMARY KEY (`id_responsable`,`nro_inventario`,`id_estado_anterior`,`id_estado_nuevo`),
+  ADD KEY `id_responsable` (`id_responsable`),
+  ADD KEY `nro_inventario` (`nro_inventario`),
+  ADD KEY `id_estado_anterior` (`id_estado_anterior`),
+  ADD KEY `id_estado_nuevo` (`id_estado_nuevo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` tinyint(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `estado_item`
+--
+ALTER TABLE `estado_item`
+  ADD CONSTRAINT `estado_item_ibfk_1` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id`),
+  ADD CONSTRAINT `estado_item_ibfk_2` FOREIGN KEY (`nro_inventario`) REFERENCES `item` (`nro_inventario`);
+
+--
+-- Filtros para la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD CONSTRAINT `movimientos_ibfk_1` FOREIGN KEY (`id_responsable`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `movimientos_ibfk_2` FOREIGN KEY (`nro_inventario`) REFERENCES `item` (`nro_inventario`),
+  ADD CONSTRAINT `movimientos_ibfk_3` FOREIGN KEY (`id_estado_anterior`) REFERENCES `estado` (`id`),
+  ADD CONSTRAINT `movimientos_ibfk_4` FOREIGN KEY (`id_estado_nuevo`) REFERENCES `estado` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
